b7781a63a0454efdef5803aa906a7a56b1d01615
Cyrill <cyrill.k@hotmail.com>
2014-04-03 16:18:32 +0200

    reverted sql scripts back to the first version

--- sql/inserttestdata.sql
+++ sql/inserttestdata.sql
@@ -10,29 +10,23 @@ delete from `case`;
 insert into user values ("cyrill", "12345");
 insert into user values ("andreas", "asdf");
 
-insert into type (name) values ( "murder");
-insert into type (name) values ( "robbery");
-
-insert into `case` (open, title, description, location, time, type_id) 
-	select TRUE, "case 1", "description 1", "location 1", "2011-12-18 13:17:17", t.id 
-	from type t 
-	where t.name = "murder";
-insert into `case` (open, title, description, location, time, type_id) 
-	select TRUE, "case 2", "description 2", "location 2", "2013-12-18 13:17:17", t.id 
-	from type t 
-	where t.name = "robbery";
+insert into `case` (open, title, description, location, time) values ( TRUE, "case 1", "description 1", "location 1", "2011-12-18 13:17:17");
+insert into `case` (open, title, description, location, time) values ( TRUE, "case 2", "description 2", "location 2", "2013-12-18 13:17:17");
 
 insert into poi (name, birthdate) values ( "alex", "2013-12-19");
 insert into poi (name, birthdate) values ( "jimmy", "2013-12-20");
 
-insert into is_linked_to (case_id, poi_id, time, end_time)
-	select c.id, p.id, "2011-12-18 13:17:17", "2011-12-21 13:17:17"
+insert into type (name) values ( "murder");
+insert into type (name) values ( "robbery");
+
+insert into is_linked_to (case_id, poi_id, type_id, time, end_time)
+	select c.id, p.id, t.id, "2011-12-18 13:17:17", "2011-12-21 13:17:17"
 	from `case` c, poi p, `type` t
-	where c.title = "case 1" and p.name = "alex";
-insert into is_linked_to (case_id, poi_id, time) 
-	select c.id, p.id, "2013-12-18 13:17:17"
+	where c.title = "case 1" and p.name = "alex" and t.name = "murder";
+insert into is_linked_to (case_id, poi_id, type_id, time) 
+	select c.id, p.id, t.id, "2013-12-18 13:17:17"
 	from `case` c, poi p, `type` t
-	where c.title = "case 2" and p.name = "alex";
+	where c.title = "case 2" and p.name = "alex" and t.name = "robbery";
 
 insert into case_note (case_id, text)
 	select c.id, "weapon used: knife"
